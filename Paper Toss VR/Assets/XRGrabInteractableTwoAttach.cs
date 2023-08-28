2da0f498ab067441569b007257e142d6a1a3c75a
CyFox66 <bam2738@gmail.com>
2023-08-28 17:15:45 -0400

    Added Paper Toss VR

---
+++ Paper Toss VR/Assets/XRGrabInteractableTwoAttach.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XRGrabInteractableTwoAttach : XRGrabInteractable
+{
+    public Transform LeftAttachTransform;
+    public Transform RightAttachTransform;
+
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        if(args.interactableObject.transform.CompareTag("Left Hand"))
+        {
+            attachTransform = LeftAttachTransform;
+        }
+        else if(args.interactableObject.transform.CompareTag("Right Hand"))
+        {
+            attachTransform = RightAttachTransform;
+        }
+        
+        base.OnSelectEntered(args);
+    }
+}
